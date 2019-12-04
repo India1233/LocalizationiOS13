d5ed414d1f304e2d380b75cc52ed6c77e83113ab
Shigasuresh@123 <shigasuresh546@gmail.com>
2019-12-04 22:36:56 +0530

    Localiztion

--- LocalizationiOS13/ViewController.swift
+++ LocalizationiOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greeting: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        greeting.setTitle(NSLocalizedString("BUTTON_TITLE", comment: ""), for: .normal)
+    }
 
+    @IBAction func greeting(_ sender: Any) {
+        setAlert()
+    }
+    
+    private func setAlert(){
+        
+        let message = NSLocalizedString("POPUP_MESSAGE_TEXT", comment: "")
+        let okBtnText = NSLocalizedString("POPUP_MESSAGE_OK_BUTTON", comment: "")
+        
+        
+        
+        
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        //create ok button
+        let acceptAction = UIAlertAction(title: okBtnText, style: .default, handler: nil)
+        alert.addAction(acceptAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
