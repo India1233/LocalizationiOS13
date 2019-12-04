9921ac18e3a018595d2245c897a9984ea661e300
Shigasuresh@123 <shigasuresh546@gmail.com>
2019-12-04 21:36:59 +0530

    Initial Commit

---
+++ LocalizationiOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizationiOS13
+//
+//  Created by shiga on 04/12/19.
+//  Copyright © 2019 Shigas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
